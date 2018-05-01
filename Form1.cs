060d6a10a577a46dbe4f542e7ddf38dfb0d6754c
Büşra ŞAHİN <busrasahin972@gmail.com>
2018-05-01 17:00:50 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formlararasigecis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad, sifre;
+            ad = textBox1.Text;
+            sifre = textBox2.Text;
+            if (ad=="Admin" && sifre=="123")
+            {
+                Form2 fr = new Form2();
+                fr.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı Kullanıcı Girişi");
+            }
+                    
+        }
+    }
+}
